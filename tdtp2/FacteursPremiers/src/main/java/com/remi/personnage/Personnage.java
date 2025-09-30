2753e459b7a6494398534152e58d38d03a3916f2
Rémi GENTIL <rgentil@iutbayonne.univ-pau.fr>
2025-09-30 09:02:19 +0200

    Test Orientation NORD ok, 1ere etape

--- tdtp2/FacteursPremiers/src/main/java/com/remi/personnage/Personnage.java
+++ tdtp2/FacteursPremiers/src/main/java/com/remi/personnage/Personnage.java
@@ -1,4 +1,13 @@
 package com.remi.personnage;
 
 public class Personnage {
+    private Orientation orientation;
+
+    public Personnage(){
+        this.orientation = Orientation.NORD;
+    }
+
+    public Orientation getOrientation() {
+        return orientation;
+    }
 }
--- tdtp2/FacteursPremiers/src/test/java/com/remi/personnage/PersonnageTest.java
+++ tdtp2/FacteursPremiers/src/test/java/com/remi/personnage/PersonnageTest.java
@@ -1,4 +1,19 @@
 package com.remi.personnage;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
 public class PersonnageTest {
+
+    @Test
+    public void nouveauPersonnageEstOrienteNord() {
+        //given
+        Personnage p = new Personnage();
+
+        //when
+        Orientation orientation = p.getOrientation();
+
+        //then
+        assertThat(orientation).isEqualTo(Orientation.NORD);
+    }
 }
