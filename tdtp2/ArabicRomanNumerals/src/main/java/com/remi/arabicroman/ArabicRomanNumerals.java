324eb28575ae845df185cdf9f850eb825279c43d
Rémi GENTIL <rgentil@iutbayonne.univ-pau.fr>
2025-09-30 14:59:01 +0200

    Test avec 10 en X OK, (et avec 5 en V.... Léger oubli.. (mais meme principe sans le test du 10 et la condition de vérification)

--- tdtp2/ArabicRomanNumerals/src/main/java/com/remi/arabicroman/ArabicRomanNumerals.java
+++ tdtp2/ArabicRomanNumerals/src/main/java/com/remi/arabicroman/ArabicRomanNumerals.java
@@ -2,6 +2,13 @@ package com.remi.arabicroman;
 
 public class ArabicRomanNumerals {
     public static String convert(int nbr) {
+        if(nbr == 5)
+        {
+            return "V";
+        }
+        else if (nbr == 10) {
+            return "X";
+        }
         return "I";
     }
 }
--- tdtp2/ArabicRomanNumerals/src/test/java/com/remi/arabicroman/ArabicRomanNumeralsTest.java
+++ tdtp2/ArabicRomanNumerals/src/test/java/com/remi/arabicroman/ArabicRomanNumeralsTest.java
@@ -6,8 +6,38 @@ import static org.assertj.core.api.Assertions.*;
 public class ArabicRomanNumeralsTest {
     @Test
     public void convertUnDonneI() {
-        String resultat = ArabicRomanNumerals.convert(1);
+        // given
+        int nombre = 1;
+
+        // when
+        String resultat = ArabicRomanNumerals.convert(nombre);
+
+        // then
         assertThat(resultat).isEqualTo("I");
     }
 
+    @Test
+    public void convertCinqDonneV() {
+        // given
+        int nombre = 5;
+
+        // when
+        String resultat = ArabicRomanNumerals.convert(nombre);
+
+        // then
+        assertThat(resultat).isEqualTo("V");
+    }
+
+    @Test
+    public void convertDixDonneX() {
+        // given
+        int nombre = 10;
+
+        // when
+        String resultat = ArabicRomanNumerals.convert(nombre);
+
+        // then
+        assertThat(resultat).isEqualTo("X");
+    }
 }
+
